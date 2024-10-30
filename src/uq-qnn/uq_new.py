20cc6ddd2b2721551e53b9d6a4a87a6ecb2f9883
nm19000 <118897012+nm19000@users.noreply.github.com>
2024-10-30 10:58:21 +0100

    data function

--- src/uq-qnn/uq_new.py
+++ src/uq-qnn/uq_new.py
@@ -9,10 +9,71 @@ import pickle
 import random as rd
 import matplotlib.pyplot as plt
 
+from collections.abc import Callable
+
 # Set random seeds for reproducibility
 np.random.seed(42)
 tf.random.set_seed(42)
 rd.seed(42)
+
+def quartic_data(input_data):
+    """ Create labels with quartic function.
+    
+    Args:
+        input_data: tf array
+    
+    Returns:
+        y: quartic function applied to input array
+    """
+
+    y = tf.pow(input_data, 4)
+
+    return y
+
+
+def get_data(n_data: int =500, sigma_noise_1: float = 0.0, datafunction: Callable = quartic_data):
+    """Define a function based toy regression dataset.
+
+    Args:
+      n_data: number of data points
+      sigma_noise_1: injected sigma noise on targets
+      datafunction: function to compute labels based on input data
+
+    Returns:
+      train_input, train_target, test_input, test_target
+    """
+    x_min = 0
+    x_max = 100
+    X_train = tf.linspace(x_min, x_max, n_data)
+
+
+    #create label noise
+    noise_1=tf.random.normal(X_train, 0, 1, tf.float32, seed=1) * sigma_noise_1
+    noise_1[X_train > gap_end] = 0 # only add noise to the right
+ 
+
+    # create simple function based labels data set and
+    # add gaussian noise
+    label_noise = noise_1
+    y_train = datafunction(X_train) + label_noise
+
+    #split training set
+    gap_start = x_min + 0.35 * (x_max - x_min)
+    gap_end = x_min + 0.6 * (x_max - x_min)
+
+    train_idx = (X_train < gap_end) & (X_train > gap_start)
+
+    # update X_train
+    X_train = X_train[~train_idx, :]
+    y_train = y_train[~train_idx, :]
+
+    #test over the whole line
+    X_test = tf.linspace(X_train.min() + X_train.min() * 0.1, X_train.max() - X_train.max() * 0.1, 500)
+    y_test = datafunction(X_test)
+
+
+    return X_train, y_train, X_test, y_test, label_noise
+
 
 def memristor_update_function(x, y1, y2):
     """
