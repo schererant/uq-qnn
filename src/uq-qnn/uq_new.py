560c85bbf908f04c049482a9b652039e9dd4b141
nm19000 <118897012+nm19000@users.noreply.github.com>
2024-11-11 18:16:52 +0100

    selective prediction and metrics compute

--- src/uq-qnn/uq_new.py
+++ src/uq-qnn/uq_new.py
@@ -11,6 +11,7 @@ import pickle
 import random as rd
 import matplotlib.pyplot as plt
 import warnings
+import uncertainty_toolbox as uct
 
 from collections.abc import Callable
 
@@ -160,6 +161,81 @@ def plot_predictions(
 
     plt.tight_layout()
     plt.show()
+
+def selective_prediction(final_predictions, targets, predictive_uncertainty, threshold: float = 0.8):
+    """ Copmutes UQ metrics
+    
+    Interpretation:
+    - final_predictions: predictions from circuit, np.array
+    - targets: data points, np.array
+    - predictive_uncertainty: predictive uncertainty from ciruit, np.array 
+    - threshold: threshold based on quantiles
+
+    Returns:
+     - final_predictions_sel: predictions from circuit, np.array
+    - targets_sel: data points, np.array
+    - predictive_uncertainty_sel: predictive uncertainty from ciruit, np.array
+    - remaining_fraction: remaining data points after selective prediction
+    """
+
+    predictive_uncertainty_quantile = np.quantile(predictive_uncertainty, threshold)
+    results_selected_mask = (predictive_uncertainty < predictive_uncertainty_quantile)
+
+    # hopefully the mask works here
+    final_predictions_sel = final_predictions[results_selected_mask]
+    targets_sel = targets[results_selected_mask]
+    predictive_uncertainty_sel = predictive_uncertainty[results_selected_mask]
+
+    remaining_fraction = len(final_predictions_sel)/len(final_predictions)
+
+    return final_predictions_sel, targets_sel, predictive_uncertainty_sel, remaining_fraction
+
+
+
+def compute_eval_metrics(final_predictions, targets, predictive_uncertainty):
+    #idea compute eval metrics for selective prediction and full version
+    """ Copmutes UQ metrics
+    
+    Interpretation:
+    - final_predictions: predictions from circuit, np.array
+    - targets: data points, np.array
+    - predictive_uncertainty: predictive uncertainty from ciruit, np.array 
+
+    Returns:
+        Dictionary containing all metrics. Accuracy metrics:  Mean average error ('mae'), Root mean squared
+        error ('rmse'), Median absolute error ('mdae'),  Mean absolute
+        relative percent difference ('marpd'), r^2 ('r2'), and Pearson's
+        correlation coefficient ('corr').
+    """
+    if len(predictive_uncertainty) >0:
+    
+        if len(final_predictions) > 0:
+            uq_metrics = uct.metrics.get_all_metrics(
+                final_predictions,
+                predictive_uncertainty,
+                targets,
+                verbose=False,
+                )
+        else:
+            uq_metrics = empty_result
+        # categories when predictive uncertainty is present
+        uq_metric_categories = [
+            "scoring_rule",
+            "avg_calibration",
+            "sharpness",
+            "accuracy",
+        ]
+
+    else:
+        # categories when no predictive uncertainty is present
+        uq_metric_categories = ["accuracy"]
+        uq_metrics = {
+            "accuracy": uct.metrics.get_all_accuracy_metrics(
+                final_predictions, targets
+            )
+        }
+
+    return uq_metrics, uq_metric_categories
 
 def memristor_update_function(x, y1, y2):
     """
